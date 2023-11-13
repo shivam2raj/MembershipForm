53213be3d50064a26e45291183e948e444ff44c9
shivam raj <shivamrrraj@gmail.com>
2023-11-13 19:15:26 +0530

    Fix line endings in app/page.js

--- app/Components/Selectplan.js
+++ app/Components/Selectplan.js
@@ -35,7 +35,7 @@ export default function ({formData,setformData}) {
     {/* 1st tile  */}
     <div className="p-2 border-2 h-32 w-28 rounded-md cursor-pointer" tabIndex={0} onFocus={() => handleButtonFocus('button1')}
         style={{ outline: formData.plan === 'button1' ? '2px solid blue' : 'none', backgroundColor: formData.plan === 'button1'? 'hsl(217, 100%, 97%)': 'white'}} >
-      <Image src={arcadeIcon} />
+      <Image src={arcadeIcon} alt="arcade icon"/>
       <p className="text-marineblue font-semibold mt-8 leading-5">Arcade</p>
       <p className="text-Coolgray text-sm">9$/mo</p>
     </div>
@@ -43,7 +43,7 @@ export default function ({formData,setformData}) {
     {/* 2nd tile  */}
     <div className="p-2 border-2 h-32 w-28 rounded-md cursor-pointer" tabIndex={0} onFocus={() => handleButtonFocus('button2')}
         style={{ outline: formData.plan === 'button2' ? '2px solid blue' : 'none', backgroundColor: formData.plan === 'button2'? 'hsl(217, 100%, 97%)': 'white'}}>
-      <Image src={advancedIcon} />
+      <Image src={advancedIcon} alt="advanced icon"/>
       <p className="text-marineblue font-semibold mt-8 leading-5">Advanced</p>
       <p className="text-Coolgray text-sm">12$/mo</p>
     </div>
@@ -51,7 +51,7 @@ export default function ({formData,setformData}) {
     {/* 3rd tile  */}
     <div className="p-2 border-2 h-32 w-28 rounded-md cursor-pointer" tabIndex={0} onFocus={() => handleButtonFocus('button3')}
         style={{ outline: formData.plan === 'button3' ? '2px solid blue' : 'none', backgroundColor: formData.plan === 'button3'? 'hsl(217, 100%, 97%)': 'white' }}>
-      <Image src={proIcon} />
+      <Image src={proIcon} alt="pro icon"/>
       <p className="text-marineblue font-semibold mt-8 leading-5">Pro</p>
       <p className="text-Coolgray text-sm">15$/mo</p>
     </div>
--- app/Components/stepbar.js
+++ app/Components/stepbar.js
@@ -15,7 +15,7 @@ export default function Stepbar (props) {
       <div className="step-bar flex flex-col gap-4 relative p-8 text-white uppercase cursor-pointer">
          
          {steps.map((steps) => (
-          <div className="" onClick={()=>handleDivClick(steps.steps)}>
+          <div key={steps.steps} className="" onClick={()=>handleDivClick(steps.steps)}>
            <div className="steps flex gap-2 leading-5 w-40 cursor-pointer">
              <div className="h-10 w-10 border rounded-full flex justify-center items-center" style={{backgroundColor: notClicked===steps.steps?"hsl(217, 100%, 97%)":" ", color:notClicked==steps.steps?'black':'white',backgroundColor: props.page===steps.steps?"hsl(217, 100%, 97%)":" ", color:props.page==steps.steps?'black':'white'}}>
                {steps.steps}
